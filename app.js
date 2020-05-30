6768666e9973e0738cf9cc164de81c8ed8233e4a
Akshay <akshaysawant8450@gmail.com>
2020-05-30 14:40:53 +0530

    form example

---
+++ app.js
@@ -0,0 +1,33 @@
+function selectedItem(value) {
+    if (value === "disable") {
+        document.querySelector(".title").style.display = "none";
+    } else if (value === "enable") {
+        document.querySelector(".title").style.display = "block";
+    }
+}
+
+function myFunction() {
+    var x = document.getElementById("myText").value;
+    document.getElementById("demo").innerHTML = x;
+}
+
+var counter = 0;
+var takeNumber = document.getElementById("submitBtn");
+takeNumber.addEventListener("click", function() {
+    counter = parseInt(document.getElementById("number").value);
+    document.getElementById("counter").innerHTML = counter;
+});
+
+var addition = document.getElementById("add");
+addition.addEventListener("click", function() {
+    counter = counter + 1;
+    document.getElementById("counter").innerHTML = counter;
+});
+
+var subtract = document.getElementById("subtract");
+subtract.addEventListener("click", function() {
+    if (counter >= 1) {
+        counter = counter - 1;
+        document.getElementById("counter").innerHTML = counter;
+    }
+});
